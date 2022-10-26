02e89802f7981425424060edd220dabbeb528202
kifuan <kifuan@foxmail.com>
2022-10-26 21:26:52 +0800

    feat: ignore empty input

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ import parse
 
 if __name__ == '__main__':
     while True:
-        code = input('> ')
+        code = input('> ').strip()
         if code in ('exit', 'quit'):
             break
+        if code == '':
+            continue
         # noinspection PyBroadException
         try:
             print(parse.parse(code))
-        except Exception as e:
+        except KeyboardInterrupt:
+            break
+        except Exception:
             traceback.print_exc()
+            print()
