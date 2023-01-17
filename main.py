0981b0df07b369cfbbebef1048a4c019e6fefc09
kifuan <kifuan@foxmail.com>
2023-01-17 14:00:43 +0800

    fix: catch KeyboardInterrupt for input function

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
-import sys
-import traceback
 import calc
 
 
 if __name__ == '__main__':
     while True:
-        code = input('> ').strip()
+        try:
+            code = input('> ').strip()
+        except KeyboardInterrupt:
+            break
         if code in ('exit', 'quit'):
             break
         if code == '':
             continue
-        # noinspection PyBroadException
-        try:
-            print(calc.calc(code))
-        except KeyboardInterrupt:
-            break
-        except Exception:
-            # To make console happy.
-            traceback.print_exc(file=sys.stdout)
+        print(calc.calc(code))
