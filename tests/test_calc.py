b7e9289f0fe1b77ca16c287133a7cbfbcdf7be69
kifuan <kifuan@foxmail.com>
2023-01-17 14:50:10 +0800

    feat: add a test for examples in the readme file

---
+++ tests/test_calc.py
@@ -0,0 +1,14 @@
+from chemcalc import calc
+
+
+def test_calc():
+    table = {
+        'NaOH': 40,
+        'H2SO4': 98,
+        '2HCl': 73,
+        '(NH4)2SO4': 132,
+        'CuSO4+5H2O': 250,
+    }
+
+    for code, value in table.items():
+        assert calc(code) == value
