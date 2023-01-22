cf61e7b2aeb3d236c25355e18dfcfbc3ec6694df
kifuan <kifuan@foxmail.com>
2023-01-22 23:01:55 +0800

    refactor: make table as a global variable

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,14 +1,15 @@
-from molar_mass_calc import calc
+from nonebot_plugin_molar_mass.calc import calc
+
+
+table = {
+    'NaOH': 40,
+    'H2SO4': 98,
+    '2HCl': 73,
+    '(NH4)2SO4': 132,
+    'CuSO4+5H2O': 250,
+}
 
 
 def test_calc():
-    table = {
-        'NaOH': 40,
-        'H2SO4': 98,
-        '2HCl': 73,
-        '(NH4)2SO4': 132,
-        'CuSO4+5H2O': 250,
-    }
-
-    for code, value in table.items():
-        assert calc(code) == value
+    for chemical, value in table.items():
+        assert calc(chemical) == value
