cf921aa0b4e502ebc8877b62f78c3c310e888f97
kifuan <kifuan@foxmail.com>
2023-01-23 10:34:19 +0800

    refactor: remove unnecessary nonebot-adapter-onebot

--- nonebot_plugin_molar_mass/__init__.py
+++ nonebot_plugin_molar_mass/__init__.py
@@ -1,6 +1,6 @@
 from nonebot import on_command, logger
 from nonebot.params import CommandArg
-from nonebot.adapters.onebot.v11.message import Message
+from nonebot.adapters import Message
 
 from .calc import calc
 
@@ -12,11 +12,14 @@ molar_mass = on_command('摩尔质量', aliases={'相对分子质量'})
 async def handle_receive_molar_mass(arg: Message = CommandArg()):
     chemical = arg.extract_plain_text()
     try:
-        await molar_mass.finish(str(calc(chemical)))
+        result = calc(chemical)
     except Exception as e:
         logger.error(f'{e.__class__.__name__}: {e}')
         await molar_mass.finish('计算出错，请检查输入格式。\n'
                                 '输入例：NaOH，H2SO4，2HCl，(NH4)2SO4，CuSO4+5H2O')
+        return
+
+    await molar_mass.finish(str(result))
 
 
 __all__ = []
